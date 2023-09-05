8c3a7bb357b514bae9c3d1b6dae0b7e4f2de46e3
wobble-house <ross@wobble.house>
2023-09-05 16:20:29 -0400

    item placement order

--- components/section.tsx
+++ components/section.tsx
@@ -151,7 +151,7 @@ export function SectionSwap2() {
               ${!section3 ? "" : "bg-[#a6d7aa] dark:bg-[#4e6950]"} 
               ${!section2 ? "" : "bg-[#687fcc] dark:bg-[#37446e]"}
               ${!section1 ? "" : "dark:bg-[#34001a] bg-[#a70054]"}`}>
-                <div className={`flex ${!section3 ? "flex-col md:flex-row": "flex-col md:flex-row-reverse"} mx-auto`}>
+                <div className={`flex ${!section3 ? "flex-col md:flex-row-reverse":"flex-col md:flex-row"} mx-auto`}>
                   <div className="flex place-items-center w-96 rounded-r-full dark:rounded-l-full dark:rounded-r-none overflow-hidden shadow-xl">
                     <Image
                       className="relative"
